63e78030c27b5cd5421ea652f4c96073850547de
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-03-15 04:28:33 +0200

    update edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://vfde-eagles-docs.netlify.com/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
